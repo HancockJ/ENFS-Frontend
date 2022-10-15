46948ad824ce63de996f6489ba41c8e96f3ce0e9
willcshapiro <willcshapiro@gmail.com>
2022-10-14 19:28:06 -0700

    added temp meta tags, image, tab name,

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -3,13 +3,30 @@ import "./AppBar.css";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import logo from "../static/images/ENFS.png";
 
 export default function AppBar() {
   return (
     <>
       <div class="AppBar">
-        <Grid container justifyContent="flex-end">
-          <Grid item xs={1}>
+       
+        <Grid container align="center" justify="center" alignItems="center">  
+          <Grid item xs={6}>
+           <Box
+           m={1}
+           //margin
+           display="flex"
+           justifyContent="flex-start"
+           alignItems="flex-start">
+           <a id="logo" href="/">
+                    {" "}
+                  </a>
+           </Box>
+         
+              
+           
+            </Grid>
+            <Grid item xs={6}>
             <Box
               m={1}
               //margin
@@ -21,6 +38,19 @@ export default function AppBar() {
             </Box>
           </Grid>
         </Grid>
+        {/* <Grid container justifyContent="flex-end">
+          <Grid item xs={1}>
+            <Box
+              m={1}
+              //margin
+              display="flex"
+              justifyContent="flex-end"
+              alignItems="flex-end"
+            >
+              <Button variant="contained">API</Button>
+            </Box>
+          </Grid>
+        </Grid> */}
       </div>
     </>
   );
