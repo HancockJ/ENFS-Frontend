fb179910c68422e6312d25b112d8b0e506884c85
jackhancock <hancockjack99@gmail.com>
2022-10-28 14:15:21 -0400

    Button board is ready for testing. Also some small formatting changes

--- src/components/ButtonBoard.js
+++ src/components/ButtonBoard.js
@@ -1,22 +1,17 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-export default function ButtonBoard() {
-    const debug = () => {
-        console.log("Clicked")
+export default function ButtonBoard({query, resetQuery}) {
+    const appendRegex = (regex) => {
+        resetQuery(query + regex)
     }
+
     return (
-        <>
-            <Grid container direction="column" align = "center" justify = "center" alignItems = "center" spacing={2}>
+            <Grid container alignItems="center" alignContent="center" flexDirection="row" spacing={2}>
                 <Grid item xs={12}>
-                    <button style={{}} onClick={debug}>Click me</button>
-                </Grid>
-                <Grid item xs={12}>
-                    <button style={{}} onClick={debug}>Click me</button>
-                </Grid>
-                <Grid item xs={12}>
-                    <button style={{}} onClick={debug}>Click me</button>
+                    <button onClick={function() {appendRegex('.')}}>Any Character</button>
+                    <button onClick={function() {appendRegex('[a-z]')}}>Any Letter </button>
+                    <button onClick={function() {appendRegex('\\d')}}>Any Number</button>
                 </Grid>
             </Grid>
-        </>
     )
 }
--- src/components/Web3Display.js
+++ src/components/Web3Display.js
@@ -2,23 +2,19 @@ import React from "react";
 import SearchBar from "./SearchBar.js";
 import Grid from "@mui/material/Grid";
 import "./Web3Display.css"
-import ButtonBoard from "./ButtonBoard.js";
 
 function Web3Display() {
   return (
-    <>
-      <div class="container">
-        <Grid container alignItems="center" justifyContent="center" >
+      <div className="container">
+        <Grid container>
           <Grid item xs={12} display={{ xs: "none", md: "block" }}>
-            <h1 class="search-bar-title" > Ethereum Name Finding Service</h1>
+            <h1 className="search-bar-title" > Ethereum Name Finding Service</h1>
           </Grid>
           <Grid item xs={12}>
             <SearchBar />
           </Grid>
-            <ButtonBoard />
         </Grid>
       </div>
-    </>
   )
 }
 
