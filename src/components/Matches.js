e93a6e934af938ef13485f6dda7e4a501ad01b63
jackhancock <hancockjack99@gmail.com>
2022-10-28 20:00:13 -0400

    Added more table formatting and also fixed some code formatting.

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -4,6 +4,8 @@ import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import config from '../config.json';
 import { DataGrid } from '@mui/x-data-grid';
+import Box from "@mui/material/Box";
+// import {createStyles, makeStyles} from "@mui/material";
 
 
 export default function Matches({ string }) {
@@ -13,19 +15,37 @@ export default function Matches({ string }) {
     const [rows,setRows] = useState([])
     const columns = [
         // {field: 'id', headerName: 'Id', width: 130},
-    {field: 'name', headerName: 'Domain' },
+    {field: 'name', headerName: 'Domain',
+        renderCell: (params) => {
+            let _url = `https://app.ens.domains/search/`+params.row.name
+            console.log("params",params)
+            return (<>
+                {<a href={_url}>{params.value}</a>}
+            </>)
+        }
+    },
     {field: 'owner', headerName: 'Owner',flex: 1,
-renderCell: (params) => {
-    let _url = `https://app.ens.domains/search/`+params.row.name
-    // console.log("params",params)
-    return (<>
-    {params.value?params.value:<a href={_url}>Available</a>}
-    
-    </>)
-}
+        renderCell: (params) => { return (
+            params.value ?
+                <p>{params.value}</p>
+                :
+                <p>Available</p>
+        )}
     },
     
     ];
+
+    // const useStyles = makeStyles(() =>
+    // createStyles({
+    //     root: {
+    //     "& .MuiDataGrid-renderingZone" : {
+    //
+    //     }
+    //     },
+    //
+    // }))
+
+
     const checkName = async (name) => {
         return axios.post(config.BACKEND_URL, { regex: name })
             .then(response => {
@@ -73,40 +93,44 @@ renderCell: (params) => {
                 temp_rows.push(temp_row)
             }
             setRows(temp_rows)
-            
-
-           
         }
     }, [names])
-    if (rows.length > 0) {
+
+
+
     return (
-        <>
-            <Grid container align="center" justify="center" alignItems="center">
-                {/* <Grid item xs={12}>
+    rows.length > 0 ?
+    <Grid container align="center" justify="center" alignItems="center">
+        <Grid item xs={12}>
+            <Box id="datagrid-table" sx={{
+                '& .available': {
+                    bgcolor: "rgb(204,255,204)",
+                },
+                '& .owned': {
+                    bgcolor: "rgb(255,204,204)",
+                }
+                }}
+            >
+                <DataGrid
+                rows={rows}
+                columns={columns}
+                autoHeight={true}
+                getRowClassName={(params) => params.row.owner ? 'owned' : 'available'}
+                />
+            </Box>
+        </Grid>
+    </Grid>
+    :
+    null
+    )
+}
+
+// Keeping this if we need later
+/* <Grid item xs={12}>
 
                     {output[0] !== '' && output.length > 0 ? <p>This string yields {output.length} combinations.</p> : null}
                     <table class="matches-table"><tr><th>ENS Domain</th><th>Availability</th></tr>
                         {Object.keys(output).map(match => {
                             return (<><tr><td key={match}>{match}.eth</td><td>{output[match].owner ? output[match].owner : <a class="purchase-link" href={"https://app.ens.domains/search/" + match}>Buy Now</a>}</td></tr></>)
                         })}</table>
-                </Grid> */}
-                <Grid item xs={12}>
-                <div id="datagrid-table">
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        // pageSize={10}
-        // rowsPerPageOptions={[5]}
-        autoHeight={true}
-        
-      />
-    </div>
-                    </Grid>
-            </Grid>
-
-
-        </>
-    )
-            }
-            else {  return (<></>)}
-}
+                </Grid> */
